c41e5e345c0fb7668d99c04ef576b6ad45c0d76b
franke2009@163.com <qxy731>
2017-09-27 00:00:38 +0800

    添加时间格式化

--- baseweb/src/com/soule/comm/tools/DateFormatDefine.java
+++ baseweb/src/com/soule/comm/tools/DateFormatDefine.java
@@ -5,5 +5,7 @@ public class DateFormatDefine {
 	public static String FORMAT_YYYYMMDD_01 = "yyyyMMdd";
 	
 	public static String FORMAT_YYYYMM_01 = "yyyyMM";
+	
+	public static String FORMAT_YYYYMMDD_02 = "yyyy-MM-dd";
 
 }
